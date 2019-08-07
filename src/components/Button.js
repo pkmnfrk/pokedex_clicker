bfd8e22a59fc38de6a8bd05976b6f3b6c31253cc
Mike Caron <caron.mike@outlook.com>
2019-08-07 11:47:08 -0400

    when a button is disabled, don't allow its click handler to be called

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,7 +4,12 @@ import './Button.css';
 
 export default function Button(props) {
     let className = "enabled";
-    if(props.disabled) className = "disabled";
+    let onClick = props.onClick;
+
+    if(props.disabled) {
+        className = "disabled";
+        onClick = null;
+    }
     className = "btn " + className;
 
     let style = {};
@@ -18,6 +23,6 @@ export default function Button(props) {
     }
 
     return (
-        <span className={className} onClick={props.onClick} style={style}>{props.children}</span>
+        <span className={className} onClick={onClick} style={style}>{props.children}</span>
     );
 };
