36b1242fe7b5c15f89c76af8f5efe22470b7e516
Mike Caron <caron.mike@outlook.com>
2019-08-14 16:45:50 -0400

    Updates

---
+++ beta/precache-manifest.9ee8f018111655ca304e03e4c444022b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3268197c9ab984bb54bd87b18cf4cc89",
+    "url": "/pokedex_clicker/index.html"
+  },
+  {
+    "revision": "b252c73f9088585902cf",
+    "url": "/pokedex_clicker/static/css/main.292b03c8.chunk.css"
+  },
+  {
+    "revision": "e289fda5c94508ebaf76",
+    "url": "/pokedex_clicker/static/js/2.f4e9e029.chunk.js"
+  },
+  {
+    "revision": "b252c73f9088585902cf",
+    "url": "/pokedex_clicker/static/js/main.55be2834.chunk.js"
+  },
+  {
+    "revision": "dd6406bd456397f3675b",
+    "url": "/pokedex_clicker/static/js/runtime~main.cf1f9a97.js"
+  }
+]);
